5a1cd8ec76d6f49f9160f4da07708af0f4913ac4
eduardojacinto <ejacinto2198@gmail.com>
2025-02-15 21:17:58 -0500

    function prototypes

--- SearchandExpand.py
+++ SearchandExpand.py
@@ -1,3 +1,18 @@
 """
 This file will hold all search functions and the point expansion function
 """
+
+def Expand():
+    pass
+
+def BreadthFirstSearch():
+    pass
+
+def DepthFirstSearch():
+    pass
+
+def GreedyBFS():
+    pass
+
+def A_Star():
+    pass
