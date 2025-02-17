a9b23b01304517cb4226bfe89262ee0d5d64d8f9
eduardojacinto <ejacinto2198@gmail.com>
2025-02-16 22:50:05 -0500

    working on Breadth search

--- search/SearchandExpand.py
+++ search/SearchandExpand.py
@@ -3,39 +3,55 @@ from grid import *
 """
 This file will hold all search functions and the point expansion function
 """
-def Valid_Move():
-    pass 
+def Valid_Move(node, enclosures):
+    for polygon in enclosures:
+        poly_points = {point.to_tuple() for point in polygon}
 
-def Expand(point):
+        if node.to_tuple() in poly_points:
+            return False
+    for dx, dy in [ (0,1), (1,0), (0, -1), (-1, 0), (1,1), (1,-1), (-1,1), (-1,-1) ]:
+        if (node.x + dx, node.y + dy) in poly_points:
+            return False
+    
+    return True
+
+def Expand(point, enclosures):
     directions = [ Point(0, 1), Point(1, 0), Point(0, -1), Point(-1, 0) ]
     children = []
     for d in directions:
         child = Point(point.x + d.x, point.y + d.y)
-        if Valid_Move(child):
+        
+        if Valid_Move(child, enclosures):
             children.append(child)
     return children
 
+def ActionCost(point, action, next_point):
+    # if next_point not on or in obstacle: return 1
+    # if next_point on or inside of turf: return 1.5
+    pass
 
-def BreadthFirstSearch(source, dest):
-    node = source
-    if node == dest:
-        return node
-    
+def BFS(source, dest, enclosures, turfs): # this works, but need to make sure that the path avoids enclosures
     frontier = Queue()
-    reached = set()
-    reached.add(node)
-    frontier.append(node)
+    frontier.push([source]) # store the paths instead of just a point
+    visited = set()
+    visited.add( source.to_tuple() )
 
-    while not frontier.isEmpty():
-        node = frontier.pop()
-        for child in Expand(node):
-            s = child
-            if s == dest:
-                return child
-            if s not in reached:
-                reached.add(s)
-                frontier.push(child)
-    return []
+    while not frontier.isEmpty():# while the frontier queue aint empty...
+
+        path = frontier.pop() # pop the next Point
+        node = path[-1]
+
+        if node == dest:
+            return path
+        
+        for child in Expand(node, enclosures):
+            
+            if child.to_tuple() not in visited:
+                visited.add( child.to_tuple() )
+                frontier.push(path + [child] )
+
+    return None
+
 
 def DepthFirstSearch():
     pass
--- search/search.py
+++ search/search.py
@@ -8,39 +8,41 @@ from grid import *
 from SearchandExpand import *
 
 def gen_polygons(worldfilepath):
-    polygons = []
-    with open(worldfilepath, "r") as f:
-        lines = f.readlines()
-        lines = [line[:-1] for line in lines]
-        for line in lines:
-            polygon = []
-            pts = line.split(';')
+    polygons = [] # will hold the shapes for enclosures or turfs
+    with open(worldfilepath, "r") as f:# read file line by line
+        lines = f.readlines() # take in each line and add it to the array of lines
+        lines = [line[:-1] for line in lines] # remove newline char at the end of each line
+        for line in lines: # go through each line
+            polygon = [] # set a list for something
+            pts = line.split(';') # split each line at the semicolon to extract the coordinate tuple and adds them to the list pts
             for pt in pts:
-                xy = pt.split(',')
-                polygon.append(Point(int(xy[0]), int(xy[1])))
-            polygons.append(polygon)
-    return polygons
+                xy = pt.split(',') # separate each points in pts into onelist of x and y val
+                polygon.append(Point(int(xy[0]), int(xy[1]))) # add the point object to the polygon list. one polygon is being created in each outer for loop iteration
+            polygons.append(polygon) # add the polygon to the polygonslist
+    return polygons # return the set of enclosures or turfs
 
 """
 so the 'node' in this project is 'Point' and it contains X and Y values. When expanding each point, the order is always up, right, down, left
 the code should implement the pseudocode
 """
-# can call BFS
-    # within BFS, call expanded to add expanded nodes to the queue? or something like that
 
 if __name__ == "__main__":
+
+    # reading enclosure and turf coordinate information
     epolygons = gen_polygons('Search/TestingGrid/world1_enclosures.txt')
     tpolygons = gen_polygons('Search/TestingGrid/world1_turfs.txt')
 
-    source = Point(24,17)
-    dest = Point(28,20)
+    # set the start and end points
+    source = Point(8,10)
+    dest = Point(43,45)
 
+    # draw the board, grids, the start point on the grid and the end point on the grid
     fig, ax = draw_board()
     draw_grids(ax)
     draw_source(ax, source.x, source.y)  # source point
     draw_dest(ax, dest.x, dest.y)  # destination point
     
-    # Draw enclosure polygons
+    # Draw enclosure polygons: points first, then the lines to connect the dots
     for polygon in epolygons:
         for p in polygon:
             draw_point(ax, p.x, p.y)
@@ -48,7 +50,7 @@ if __name__ == "__main__":
         for i in range(0, len(polygon)):
             draw_line(ax, [polygon[i].x, polygon[(i+1)%len(polygon)].x], [polygon[i].y, polygon[(i+1)%len(polygon)].y])
     
-    # Draw turf polygons
+    # Draw turf polygons: first the dots, then the lines to connect
     for polygon in tpolygons:
         for p in polygon:
             draw_green_point(ax, p.x, p.y)
@@ -57,10 +59,13 @@ if __name__ == "__main__":
             draw_green_line(ax, [polygon[i].x, polygon[(i+1)%len(polygon)].x], [polygon[i].y, polygon[(i+1)%len(polygon)].y])
 
     #### Here call your search to compute and collect res_path
-    
+    # seems like the search is supposed to just collect points. The for loop at the end is drawing the line for the path.
+    # the res_path var is a list that holds Point variables
 
-    res_path = [Point(24,17), Point(25,17), Point(26,17), Point(27,17),  
-                Point(28,17), Point(28,18), Point(28,19), Point(28,20)]
+    res_path = BFS(source, dest, epolygons, tpolygons)
+
+    # res_path = [Point(24,17), Point(25,17), Point(26,17), Point(27,17),  
+    #             Point(28,17), Point(28,18), Point(28,19), Point(28,20)]
     
     for i in range(len(res_path)-1):
         draw_result_line(ax, [res_path[i].x, res_path[i+1].x], [res_path[i].y, res_path[i+1].y])
